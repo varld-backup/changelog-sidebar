35fa02492383af5a0358a43185611dc76d57625e
Tobias Herber <tobias@herber.space>
2020-01-18 12:18:06 +0100

    Add sidebar sdk

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import CoreSDK, { CtorOpts } from 'changelog-core';
+
+class BarSDK extends CoreSDK {
+  private element: HTMLDivElement;
+  public isOpen: boolean;
+
+  constructor(opts: CtorOpts) {
+    super(Object.assign(opts, {
+      mode: 'bar'
+    }));
+
+    // Create a container-element for the iframe
+    this.element = document.createElement('div');
+    this.element.setAttribute('changes-bar', '');
+    this.element.style.position = 'fixed';
+    this.element.style.top = '0px';
+    this.element.style.bottom = '0px';
+    this.element.style.width = '350px';
+    this.element.style.right = '-350px';
+    this.element.style.zIndex = '50000';
+    this.element.style.transition = 'all .3s ease-in-out';
+    this.element.style.boxShadow = '0 1px 30px rgba(0, 0, 0, 0.09)';
+    this.element.appendChild(this.iframe);
+    document.body.appendChild(this.element);
+
+    this.isOpen = false;
+
+    this.on('message', message => {
+      if (message.type == 'changes-close') {
+        this.close();
+      }
+    });
+  }
+
+  public open() {
+    this.element.style.right = '0px';
+    this.isOpen = true;
+  }
+
+  public close() {
+    this.element.style.right = '-350px';
+    this.isOpen = false;
+  }
+
+  public toggle() {
+    if (this.isOpen) {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+}
+
+export default BarSDK;
